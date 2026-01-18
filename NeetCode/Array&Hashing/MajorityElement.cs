457313c0d0f23572c7cc19eeb93f47af4db3b564
yassin <yassinbenyahia17@gmail.com>
2026-01-17 22:52:18 -0500

    Majority element

---
+++ NeetCode/Array&Hashing/MajorityElement.cs
@@ -0,0 +1,30 @@
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        Array.Sort(nums);
+        return nums[nums.Length / 2];
+    }
+}
+
+//Alternative and better way :
+
+public class Solution
+{
+    public int MajorityElement(int[] nums)
+    {
+        int res = 0, count = 0;
+
+        foreach (int num in nums)
+        {
+            if (count == 0)
+            {
+                res = num;
+            }
+            count += (num == res) ? 1 : -1;
+        }
+
+        return res;
+    }
+}
