1d8e4c16fe4ad0142bcb725ab403408975022c9c
yassin <yassinbenyahia17@gmail.com>
2026-01-29 17:03:18 -0500

    3Sum

---
+++ NeetCode/TwoPointer/3Sum.cs
@@ -0,0 +1,35 @@
+public class Solution {
+    public IList<IList<int>> ThreeSum(int[] nums) {
+          Array.Sort(nums);
+         IList<IList<int>> res = new List<IList<int>>();
+        for(int i=0;i<nums.Length;i++)
+        { if(i>0 && nums[i]==nums[i-1]) continue;
+           int l=i+1;
+           int r=nums.Length-1;
+
+           while(l<r)
+           {
+            int sum=nums[i]+nums[l]+nums[r];
+            if(sum>0)
+            {
+              r--;
+            }
+            else if(sum<0)
+            {
+               l++;
+            }
+            else
+            {
+            res.Add(new List<int> { nums[i], nums[l], nums[r] });
+                l++;
+                r--;
+            while(nums[l]==nums[l-1]&&l<r)
+            {    
+                l++;
+            }
+            }
+           }
+        }
+        return res;
+    }
+}
